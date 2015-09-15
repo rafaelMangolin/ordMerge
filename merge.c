e6b37efed2f684a58ec385e1ff9c572737d3a49d
rafaelMangolin <rbmangolin@gmail.com>
2015-09-15 12:23:15 -0300

    corrigido para array de ponteiros

--- merge.c
+++ merge.c
@@ -2,21 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAXINT 9999999999
-
-int qntdArqUso = 0;
-
 struct registro{
     int id;
-    char desc[55];
+    char descricao[55];
 }registro;
 
 int main(){
     int tamanhoEntrada,tamanhoSaida;
-    char *nome;
+    char nome[300];
 
     printf("Digite o nome do arquivo:");
-    gets(nome);
+    scanf("%s[^\\n]",nome);
 
     printf("Digite o valor do buffer de entrada:");
     scanf("%i",&tamanhoEntrada);
@@ -28,17 +24,17 @@ int main(){
 }
 
 void merge(int tamanhoBufferEntrada,int tamanhoBufferSaida,char *nome){
-    //Declarações
     int qntdRegBufferSaida = 0;
     int regCountBreak = 0;
     int nomeArqIncr = 0;
     int regcont = 0;
-    int arr_rrn[2000];
+    int qntdArqUso = 0;
+    FILE *arrPonteiro[2000];
 
     struct registro buffer[tamanhoBufferEntrada];
     struct registro bufferSaida[tamanhoBufferSaida];
 
-    FILE *file,*fileWriteFinal;
+    FILE *file,*ponteiroSaida;
 
     if ((file = fopen(nome, "r")) == NULL) {
        printf("Tentativa de abrir o arquivo invalida\n");
@@ -57,7 +53,7 @@ void merge(int tamanhoBufferEntrada,int tamanhoBufferSaida,char *nome){
         FILE *fileWrite;
         char nomeCorrida[50];
 
-        sprintf(nomeCorrida, "corrida_%i.txt", nomeArqIncr++);
+        sprintf(nomeCorrida, "corrida%i.txt", nomeArqIncr++);
 
         if ((fileWrite = fopen(nomeCorrida, "w")) == NULL) {
            printf("Tentativa de abrir o arquivo invalida\n");
@@ -65,13 +61,15 @@ void merge(int tamanhoBufferEntrada,int tamanhoBufferSaida,char *nome){
         }
         printf("\n\nCorrida numero %i \n", nomeArqIncr-1);
         int i;
-        for(i = 0 ;i < tamanhoBufferEntrada;i++){
-            printf("Chave: %i, texto: %s \n",buffer[i].id,buffer[i].desc);
-        }
-
         fwrite(buffer,tamanhoBufferEntrada,sizeof(registro),fileWrite);
         fclose(fileWrite);
+        for(i = 0 ;i < tamanhoBufferEntrada;i++){
+            printf("Chave: %i, texto: %s \n",buffer[i].id,buffer[i].descricao);
+        }
     }
+
+
+
     qntdArqUso = nomeArqIncr;
     printf("Quantidade de corridas geradas %i, reads %i, writes %i",qntdArqUso,qntdArqUso,qntdArqUso);
 
@@ -80,24 +78,44 @@ void merge(int tamanhoBufferEntrada,int tamanhoBufferSaida,char *nome){
         exit(1);
     }
 
-    if((fileWriteFinal = fopen("arquivo_saida.txt", "w")) == NULL) {
+    if((ponteiroSaida = fopen("arquivo_saida.txt", "w")) == NULL) {
        printf("Tentativa de abrir o arquivo invalida\n");
        exit(1);
     }
-    fclose(fileWriteFinal);
 
     int i;
     for(i = 0 ;i < nomeArqIncr ; i++){
-        arr_rrn[i] = 0;
-        fromBufToFile(i,arr_rrn,buffer);
+        char nomeCorrida[50];
+        sprintf(nomeCorrida, "corrida%i.txt", i);
+        if ((arrPonteiro[i] = fopen(nomeCorrida, "r+")) == NULL) {
+           printf("Tentativa de abrir o arquivo invalida\n");
+           exit(1);
+        }
+        fromBufToFile(i,arrPonteiro,buffer,&qntdArqUso);
     }
 
     while (qntdArqUso > 0 ){
-        fromBufferToOutBuffer(buffer,bufferSaida,arr_rrn,tamanhoBufferEntrada,tamanhoBufferSaida,&qntdRegBufferSaida);
-
+        fromBufferToOutBuffer(buffer,bufferSaida,arrPonteiro,tamanhoBufferEntrada,tamanhoBufferSaida,&qntdRegBufferSaida,ponteiroSaida,&qntdArqUso);
     }
 
+    fclose(ponteiroSaida);
     fclose(file);
+
+    FILE *fp;
+    if ((fp = fopen("arquivo_saida.txt", "r")) == NULL) {
+           printf("Erro na abertura do arquivo --- programa abortado\n");
+           exit(1);
+        }
+        int h =0,x = 0;
+        struct registro cx;
+        fseek(fp,0,0);
+         h = fread(&cx,1,sizeof(registro),fp);
+    while(h!= 0){
+        printf("ARQ ID: %i contador : %i \n" , cx.id , x++) ;
+        h = fread(&cx,1,sizeof(registro),fp);
+    }
+
+    fclose(fp);
 }
 
 int encontraMenor(struct registro *buffer,int length){
@@ -110,51 +128,37 @@ int encontraMenor(struct registro *buffer,int length){
     return menor;
 }
 
-void fromBufferToOutBuffer(struct registro *buffer, struct registro *bufferSaida,int arr_rrn[],int tambuffer,int tambufferSaida,int *contSaida){
+void fromBufferToOutBuffer(struct registro *buffer, struct registro *bufferSaida,FILE* arrPonteiro[],int tambuffer,int tambufferSaida,int *contSaida,FILE *ponteiroSaida, int *qntdArqUso){
     int menor = encontraMenor(buffer,tambuffer);
 
     bufferSaida[*contSaida] = buffer[menor];
     *contSaida += 1;
 
-    fromBufToFile(menor,arr_rrn,buffer);
+    fromBufToFile(menor,arrPonteiro,buffer,qntdArqUso);
 
     if(tambufferSaida == *contSaida){
-        FILE *fileWriteOutput;
-        if ((fileWriteOutput = fopen("arquivo_saida.txt", "r+")) == NULL) {
-           printf("Tentativa de abrir o arquivo invalida\n");
-           exit(1);
-        }
-        fseek(fileWriteOutput,0,SEEK_END);
-
-        fwrite(bufferSaida,tambufferSaida,sizeof(registro),fileWriteOutput);
+        fwrite(bufferSaida,tambufferSaida,sizeof(registro),ponteiroSaida);
         *contSaida = 0;
-        fclose(fileWriteOutput);
     }
 }
 
 
-void fromBufToFile(int index, int arr_rrn[],struct registro *buffer ){
+void fromBufToFile(int indice, FILE* arrRegistros[],struct registro *buffer ,int *qntdArqUso){
         FILE *fileRead;
         char nomeCorrida[50];
         struct registro str_aux;
         int retornoQuantidade = -1;
 
-        sprintf(nomeCorrida, "corrida_%i.txt", index);
 
-        if ((fileRead = fopen(nomeCorrida, "r+")) == NULL) {
-           printf("Tentativa de abrir o arquivo invalida\n");
-           exit(1);
-        }
 
-        fseek(fileRead,arr_rrn[index]++ * sizeof(registro),SEEK_SET);
-        //Passar o tamanho do buffer no arquivo
-        retornoQuantidade = fread(&str_aux,1,sizeof(registro),fileRead);
+        retornoQuantidade = fread(&str_aux,1,sizeof(registro),arrRegistros[indice]);
 
         if(retornoQuantidade == 0 ){
-            buffer[index].id = 9999999999;
-            qntdArqUso--;
+            fclose(arrRegistros[indice]);
+            buffer[indice].id = 9999999999;
+            *qntdArqUso-=1;
         }else{
-            buffer[index] = str_aux;
+            buffer[indice] = str_aux;
         }
 }
 
